d68aedd18f9cf7b74186e07644377b06e57f181e
Michal Jurek <s8mijure@stud.uni-saarland.de>
2020-08-10 18:46:19 +0200

    Ich habe fertig

--- src/main/java/com/pseuco/np20/simulation/rocket/Patch.java
+++ src/main/java/com/pseuco/np20/simulation/rocket/Patch.java
@@ -1,32 +1,120 @@
 package com.pseuco.np20.simulation.rocket;
 
-import com.pseuco.np20.model.Rectangle;
+import com.pseuco.np20.model.*;
 import com.pseuco.np20.simulation.common.Context;
 import com.pseuco.np20.simulation.common.Person;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Patch extends Thread implements Context
 {
-    Rectangle contextGrid;
-    List<Rectangle> obstacles;
+    private boolean firstRun;
 
+    private final Scenario scenario;
 
+    private final Rectangle patchGrid;
+    private final Rectangle allGrid;
 
-    @Override
-    public Rectangle getGrid() {
-        return contextGrid;
+    private final List<Person> allPopulation;
+    private final List<Person> population;
+
+    private final Map<String, List<Statistics>> statistics;
+    private final List<TraceEntry> traces;
+
+    public Patch(Scenario pScenario, List<Person> pAllPopulation, Rectangle pPatchGrid)
+    {
+        population = new ArrayList<>();
+        statistics = new HashMap<>();
+        traces = new LinkedList<>();
+
+        firstRun = true;
+
+        scenario = pScenario;
+        allPopulation = pAllPopulation;
+        patchGrid = pPatchGrid;
+        allGrid = null;
+    }
+
+    // We take the people we need. No Data race cause shared allPopulation list is only read from.
+    private void populate()
+    {
+        for(Person person : allPopulation)
+        {
+            if(patchGrid.contains(person.getPosition()));
+            {
+                population.add(person.clone(this));
+            }
+        }
+    }
+
+    // We initialize the map we use to collect the necessary statistics
+    private void initStatistics()
+    {
+        for(String queryKey : scenario.getQueries().keySet())
+        {
+            statistics.put(queryKey, new LinkedList<>());
+        }
+    }
+
+    // We collect the statistics for our patch for the current tick
+    // At the end, Rocket will add those up
+    private void extendStatistics()
+    {
+        for(Map.Entry<String, Query> entry : scenario.getQueries().entrySet()) {
+            final Query query = entry.getValue();
+            statistics.get(entry.getKey()).add(new Statistics(
+                    population.stream().filter(
+                            (Person person) -> person.isSusceptible() && query.getArea().contains(person.getPosition())
+                    ).count(),
+                    population.stream().filter(
+                            (Person person) -> person.isInfected() && query.getArea().contains(person.getPosition())
+                    ).count(),
+                    population.stream().filter(
+                            (Person person) -> person.isInfectious() && query.getArea().contains(person.getPosition())
+                    ).count(),
+                    population.stream().filter(
+                            (Person person) -> person.isRecovered() && query.getArea().contains(person.getPosition())
+                    ).count()
+            ));
+        }
+    }
+
+    // We extend the the trace for the current tick
+    private void extendTraces()
+    {
+        if(scenario.getTrace())
+        {
+            traces.add(new TraceEntry(population.stream().map(Person::getInfo).collect(Collectors.toList())));
+        }
     }
 
     @Override
-    public List<Rectangle> getObstacles() {
-        return obstacles;
+    public Rectangle getGrid()
+    {
+        return allGrid;
+    }
+
+    @Override
+    public List<Rectangle> getObstacles()
+    {
+        return scenario.getObstacles();
     }
 
     @Override
     public List<Person> getPopulation()
     {
-        //combine both lists
-        return null;
+        return population;
+    }
+
+    @Override
+    public void run()
+    {
+        if(firstRun)
+        {
+            populate();
+            initStatistics();
+            firstRun = false;
+        }
     }
 }
--- src/main/java/com/pseuco/np20/simulation/rocket/Rocket.java
+++ src/main/java/com/pseuco/np20/simulation/rocket/Rocket.java
@@ -1,10 +1,12 @@
 package com.pseuco.np20.simulation.rocket;
 
-import com.pseuco.np20.model.Output;
-import com.pseuco.np20.model.Scenario;
+import com.pseuco.np20.model.*;
+import com.pseuco.np20.simulation.common.Person;
 import com.pseuco.np20.simulation.common.Simulation;
 import com.pseuco.np20.validator.InsufficientPaddingException;
 import com.pseuco.np20.validator.Validator;
+
+import java.util.*;
 
 
 /**
@@ -16,7 +18,15 @@ import com.pseuco.np20.validator.Validator;
  */
 public class Rocket implements Simulation
 {
+    private final Scenario scenario;
+    private final List<Person> allPopulation;
+    private final List<Rectangle> patches;
+
+    private final int padding;
     private final int ticksAllowed;
+
+    private final Map<String, List<Statistics>> statistics;
+    private final List<TraceEntry> traces;
 
     /**
      * Constructs a rocket with the given parameters.
@@ -32,22 +42,90 @@ public class Rocket implements Simulation
      * padding is below this minimal required padding.
      * </p>
      *
-     * @param scenario The scenario to simulate.
-     * @param padding The padding to be used.
-     * @param validator The validator to be called.
+     * @param pScenario The scenario to simulate.
+     * @param pPadding The padding to be used.
+     * @param pValidator The validator to be called.
      */
-    public Rocket(Scenario scenario, int padding, Validator validator) throws InsufficientPaddingException
+    public Rocket(Scenario pScenario, int pPadding, Validator pValidator) throws InsufficientPaddingException
     {
-        ticksAllowed = 5;
+        allPopulation = new ArrayList<>();
+        patches = new LinkedList<>();
+        statistics = new HashMap<>();
+        traces = new LinkedList<>();
+
+        scenario = pScenario;
+        padding = pPadding;
+
+        ticksAllowed = 5; //TODO
+        if(ticksAllowed <= 0)
+        {
+            throw new InsufficientPaddingException(padding);
+        }
+
+        populate();
+        initStatistics();
+        initThreads();
+    }
+
+    // We create the initial allPopulation list here so that everyone has a unique id.
+    private void populate()
+    {
+        int id = 0;
+        for(PersonInfo personInfo : scenario.getPopulation())
+        {
+            allPopulation.add(new Person(id, null, scenario.getParameters(), personInfo));
+            id++;
+        }
+    }
+
+    // We initialize the map we use to collect the necessary statistics
+    private void initStatistics()
+    {
+        for(String queryKey : scenario.getQueries().keySet())
+        {
+            statistics.put(queryKey, new LinkedList<>());
+        }
+    }
+
+    private void initThreads()
+    {
+        Iterator<Rectangle> patchesIterator = Utils.getPatches(scenario);
+        int patchesNum = 0;
+        while(patchesIterator.hasNext())
+        {
+            Rectangle patch = patchesIterator.next();
+
+            if(patch.getTopLeft().getY() >= 0)
+            {
+                Rectangle pTop = new Rectangle(
+                        new XY(patch.getTopLeft().getX(), Math.max(0, patch.getTopLeft().getY() - padding)),
+                        new XY(patch.getSize().getX(), (patch.getTopLeft().getY() - Math.max(0, patch.getTopLeft().getY() - padding))));
+            }
+            if(patch.getTopLeft().getX() >= 0)
+            {
+                
+            }
+            if(patch.getTopLeft().getY() >= 0 && patch.getTopLeft().getX() >= 0)
+            {
+
+            }
+
+            patches.add(patchesIterator.next());
+            patchesNum++;
+        }
+
+
     }
 
     @Override
-    public Output getOutput() {
+    public Output getOutput()
+    {
         throw new RuntimeException("not implemented");
     }
 
     @Override
-    public void run() {
+    public void run()
+    {
         throw new RuntimeException("not implemented");
     }
 }
